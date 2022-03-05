7d8ce251568589506803e5b9faeeab44be0a84ff
Arinjaychaudhary <76354318+Arinjaychaudhary@users.noreply.github.com>
2022-03-05 08:54:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function bedroom(){
+    window.location="bedroom.html";
+}
+
+function tv(){
+    window.location="tv.html";
+}
+
+function bottle(){
+    window.location="bottle.html";
+}
+
+function fruit(){
+    window.location="fruit.html";
+}
+
+function back(){
+window.location="index.html";
+}
